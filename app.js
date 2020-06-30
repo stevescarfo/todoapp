b5ece0310932621148487b42f2295083b9444d5d
stevescarfo <steve@stevescarfo.com>
2020-06-30 19:53:25 -0400

    To Do before delete created

--- app.js
+++ app.js
@@ -1,9 +1,72 @@
-function add() {
-    var textBoxContent = document.getElementById('itemToAdd').value;
-    localStorage.setItem('todoItem',JSON.stringify(textBoxContent));
+show();
 
-    console.log('The value of textBoxContent is ' + textBoxContent);
-}
+function get_todos() {
+    //grab data from local storage
+    var todos = new Array();
+
+    var todosString = localStorage.getItem("todos");
+
+    // convert to JSON
+
+    if (todosString !== null) {
+
+        todos = JSON.parse(todosString);
+
+    };
+
+    return todos;    
+}; // end function
+
+
+
+function add() {
+
+    var todos = get_todos();
+
+    var textBoxContent = document.getElementById('itemToAdd').value;
+
+    todos.push(textBoxContent);
+
+    localStorage.setItem('todos',JSON.stringify(todos));
+
+    show();
+ }; // end function
+
+
+
+function  show() {
+    var todos = get_todos();
+
+    var html = '<ul>'   // add each todo item to the string as a <li>
+
+    for ( var i = 0; i < todos.length ; i++)
+    {
+        html += '<li>' + todos[i] + '&nbsp;&nbsp;&nbsp;<button>Delete Item</button></li>';
+
+    }
+
+    html += '</ul>';
+
+    document.getElementById('todos').innerHTML = html;
+}; // end function
+
+
 
 
 document.getElementById('addTodo').addEventListener('click', add);
+
+
+
+
+/* Notes on deleting todo items
+
+1. you need a unique ID for each item
+2. you need a delete button - that can be accessed individually in JS (ID)
+3. you can use the ID for each item to give its button a unique ID.
+4. you can grab the ID for the todo that is to be deleted using JS.
+5. remember you can create HTML using strings including IDs.
+6. to remove an element from an array use the function slice().
+
+buttons have to have an event listener - those will be created at the time you dynamically create a button.
+
+*/
